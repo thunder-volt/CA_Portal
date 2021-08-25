ebcde2e5ab0a9beed866e1f158e035ac028a5a5d
Prithivi-Raj <na19b052@smail.iitm.ac.in>
2021-08-25 19:59:16 +0530

    coloured

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
 import React from 'react';
 // import logo from './logo.svg';
-import './App.css';
-import Nav from "./Nav";
-import Body from "./Body";
+// import './App.css';
+// import Nav from "./Nav";
+// import Body from "./Body";
 
-function App() {
-  return (
-    <div className="App">
+// function App() {
+//   return (
+//     <div className="App">
       
-    <Nav/>
-    <Body/>    
+//     <Nav/>
+//     <Body/>    
    
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
-  );
-}
+//       {/* <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header> */}
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
 
 // import Nav from "./Nav";
 // import Body from "./Body";
@@ -44,3 +44,16 @@ export default App;
 // }
 
 // export default App
+import Nav from "./Nav";
+import Body from "./Body";
+
+const App = () => {
+  return (
+    <div>
+    <Nav/>
+    <Body/>    
+    </div>
+  )
+}
+
+export default App
