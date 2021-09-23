5c2bfd39ff53691951f288f1b1e419d6ef708dbe
YOGHESH04 <bs19b034@smail.iitm.ac.in>
2021-09-23 16:16:20 +0530

    MyApplication_updated

--- src/Application.tsx
+++ src/Application.tsx
@@ -1,185 +1,205 @@
 import { stringify } from "querystring";
 import React from "react";
 import { useHistory, useParams } from "react-router";
-import "./Application.css";
+import "./MyApplication.css";
 import { FaUserCircle } from "react-icons/fa";
 import images1 from "./assets/demopic.jpg";
-import {ApplicationResultInput, useApplicationResultMutation, useGetQuestionnaireByUserIdQuery, useGetUserQuery, UserRole} from "./generated"
+import {
+  ApplicationResultInput,
+  useApplicationResultMutation,
+  useGetQuestionnaireByUserIdQuery,
+  useGetUserQuery,
+  UserRole,
+} from "./generated";
 
 function Application() {
-  let {id}:any = useParams()
-  console.log(id)
-  const {data,loading,error} = useGetQuestionnaireByUserIdQuery({variables: {userid: id}})
-  const [applicationMutation] = useApplicationResultMutation()
-  const {data: getUserData, loading : getUserLoading, error : getUserError} = useGetUserQuery({variables: {userId: id}})
-  const history = useHistory()
-  const [coord, setCoord] = React.useState("")
-  var [input, setInput] = React.useState<ApplicationResultInput>({coord:"",id:id,isSelected:false})
-  console.log(data)
-  console.log(getUserData?.getUser?.role)
+  let { id }: any = useParams();
+  console.log(id);
+  const { data, loading, error } = useGetQuestionnaireByUserIdQuery({
+    variables: { userid: id },
+  });
+  const [applicationMutation] = useApplicationResultMutation();
+  const {
+    data: getUserData,
+    loading: getUserLoading,
+    error: getUserError,
+  } = useGetUserQuery({ variables: { userId: id } });
+  const history = useHistory();
+  const [coord, setCoord] = React.useState("");
+  var [input, setInput] = React.useState<ApplicationResultInput>({
+    coord: "",
+    id: id,
+    isSelected: false,
+  });
+  console.log(data);
+  console.log(getUserData?.getUser?.role);
 
   const accept = async () => {
-    setCoord(coord)
-    await setInput(prevState => {return {...prevState, isSelected: true, id: id,coord: coord}})
-    try
-    {
-      console.log(coord)
-      const resp = await applicationMutation({variables: {data: {isSelected: true, id:id, coord:coord}}})
-      console.log(resp.data?.applicationResult)
-      console.log(getUserData?.getUser?.role)
+    setCoord(coord);
+    await setInput((prevState) => {
+      return { ...prevState, isSelected: true, id: id, coord: coord };
+    });
+    try {
+      console.log(coord);
+      const resp = await applicationMutation({
+        variables: { data: { isSelected: true, id: id, coord: coord } },
+      });
+      console.log(resp.data?.applicationResult);
+      console.log(getUserData?.getUser?.role);
+    } catch (err) {
+      window.alert(err);
     }
-    catch(err)
-    {
-      window.alert(err)
-    }
-    history.push('/search')
-  }
+    history.push("/search");
+  };
   const reject = async () => {
-    setInput({...input, isSelected: false, id: id, coord:coord})
-    try 
-    {
-      const resp = await applicationMutation({variables: {data: input}})
-      console.log(resp.data?.applicationResult)
-      console.log(getUserData?.getUser?.role)
+    setInput({ ...input, isSelected: false, id: id, coord: coord });
+    try {
+      const resp = await applicationMutation({ variables: { data: input } });
+      console.log(resp.data?.applicationResult);
+      console.log(getUserData?.getUser?.role);
+    } catch (err) {
+      window.alert(err);
     }
-    catch(err)
-    {
-      window.alert(err)
-    }
-    history.push('/search')
-  }
+    history.push("/search");
+  };
 
-  if(getUserData)
-  return (
-    <div className="Application">
-      <div>
-        <h1>APPLICATION</h1>
-        <div className="flexdis">
-          <div>
-            <h2>PROFILE DETAILS :</h2>
-            <h2 className="details">{getUserData.getUser?.name}</h2>
-            <h2 className="details">{getUserData.getUser?.email}</h2>
+  if (getUserData)
+    return (
+      <div className="Application">
+        <div>
+          <h1>APPLICATION</h1>
+          <div className="flexdis">
+            <div>
+              <h2>PROFILE DETAILS :</h2>
+              <h2 className="details">{getUserData.getUser?.name}</h2>
+              <h2 className="details">{getUserData.getUser?.email}</h2>
+            </div>
+            <div>
+              <FaUserCircle className="photo" size="30%"></FaUserCircle>
+              {/* <img className="photo" src={FaUserCircle} alt="" /> */}
+            </div>
           </div>
-          <div>
-            <FaUserCircle className="photo" size="30%"></FaUserCircle>
-            {/* <img className="photo" src={FaUserCircle} alt="" /> */}
+          <div className="column">
+            <div>
+              <ul className="ul11">
+                <li>
+                  <b>COLLEGE NAME :</b> {data?.getQuestionnaireByUserId.college}
+                </li>
+                <li>
+                  <b>COLLEGE ADDRESS :</b>{" "}
+                  {data?.getQuestionnaireByUserId.collegeaddress}
+                </li>
+                <li>
+                  <b>CITY :</b> {data?.getQuestionnaireByUserId.city}
+                </li>
+                <li>
+                  <b>STATE :</b> {data?.getQuestionnaireByUserId.state}
+                </li>
+                <li>
+                  <b>DEGREE:</b> {data?.getQuestionnaireByUserId.Degree}
+                </li>
+                <li>
+                  <b>BRANCH :</b> {data?.getQuestionnaireByUserId.branch}
+                </li>
+                <li>
+                  <b>YEAR :</b> {data?.getQuestionnaireByUserId.year}
+                </li>
+              </ul>
+            </div>
+            <div>
+              <ul>
+                <li>
+                  <b>POSTAL ADDRESS:</b>{" "}
+                  {data?.getQuestionnaireByUserId.postaladdress}
+                </li>
+                <li>
+                  <b>CITY :</b> {data?.getQuestionnaireByUserId.pcity}
+                </li>
+                <li>
+                  <b>STATE :</b> {data?.getQuestionnaireByUserId.pstate}
+                </li>
+                <li>
+                  <b>PINCODE :</b> {data?.getQuestionnaireByUserId.pincode}
+                </li>
+                <li>
+                  <b>CONTACT NUMBER :</b>{" "}
+                  {data?.getQuestionnaireByUserId.contactno}
+                </li>
+                <li>
+                  <b>WHATSAPP NUMBER :</b>{" "}
+                  {data?.getQuestionnaireByUserId.whatsappno}
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-        <div className="column">
-          <div>
-            <ul className="ul11">
-              <li>
-                <b>COLLEGE NAME :</b> {data?.getQuestionnaireByUserId.college}
-              </li>
-              <li>
-                <b>COLLEGE ADDRESS :</b> {data?.getQuestionnaireByUserId.collegeaddress}
-              </li>
-              <li>
-                <b>CITY :</b> {data?.getQuestionnaireByUserId.city}
-              </li>
-              <li>
-                <b>STATE :</b> {data?.getQuestionnaireByUserId.state}
-              </li>
-              <li>
-                <b>DEGREE:</b> {data?.getQuestionnaireByUserId.Degree}
-              </li>
-              <li>
-                <b>BRANCH :</b> {data?.getQuestionnaireByUserId.branch}
-              </li>
-              <li>
-                <b>YEAR :</b> {data?.getQuestionnaireByUserId.year}
-              </li>
-            </ul>
-          </div>
-          <div>
-            <ul>
-              <li>
-                <b>POSTAL ADDRESS:</b> {data?.getQuestionnaireByUserId.postaladdress}
-              </li>
-              <li>
-                <b>CITY :</b> {data?.getQuestionnaireByUserId.pcity}
-              </li>
-              <li>
-                <b>STATE :</b> {data?.getQuestionnaireByUserId.pstate}
-              </li>
-              <li>
-                <b>PINCODE :</b> {data?.getQuestionnaireByUserId.pincode}
-              </li>
-              <li>
-                <b>CONTACT NUMBER :</b> {data?.getQuestionnaireByUserId.contactno}
-              </li>
-              <li>
-                <b>WHATSAPP NUMBER :</b> {data?.getQuestionnaireByUserId.whatsappno}
-              </li>
-            </ul>
+        <div className="container">
+          <h3 className="questionarise">OTHER GENERAL QUESTIONS</h3>
+          <div className="questions">
+            <h3>Were you a Shaastra CA previously?</h3>
+            <div>
+              <p>{data?.getQuestionnaireByUserId.Q1 === "NO" ? "NO" : "YES"}</p>
+            </div>
+            <p></p>
+            <h3>If Your answer to the above question is yes, which year ?</h3>
+            <p>
+              {data?.getQuestionnaireByUserId.Q1 === "NO"
+                ? "-"
+                : data?.getQuestionnaireByUserId.Q1}
+            </p>
+            <h3>Which social media sites do you use? </h3>
+            <p>{data?.getQuestionnaireByUserId.Q2}</p>
+            <h3> Facebook Profile Link: </h3>
+            <p>
+              {data?.getQuestionnaireByUserId.Q3}
+              <link rel="stylesheet" href="styles.css" />
+            </p>
+            <h3> Why do you wish to become a Campus Ambassador? </h3>
+            <p>{data?.getQuestionnaireByUserId.Q4}</p>
+            <h3>
+              Why do you think you are the right candidate for this position?
+            </h3>
+            <p>{data?.getQuestionnaireByUserId.Q5}</p>
+            <h3>
+              Do you have any past experience in handling Positions of
+              Responsibility?{" "}
+            </h3>
+            <p>{data?.getQuestionnaireByUserId.Q6}</p>
+            <label htmlFor="sfafaferfaef"></label>
+            <select
+              onChange={async (e) => {
+                await setCoord(e.target.value);
+                console.log(coord);
+              }}
+            >
+              <option value="Anshid">Anshid</option>
+              <option value="Samrudha Lakshmi">Samrudha Lakshmi</option>
+              <option value="Poojitha R">Poojitha R</option>
+              <option value="Sukriti">Sukriti </option>
+              <option value="Vishwa">Vishwa</option>
+              <option value="Pavan Kumar">Pavan Kumar</option>
+              <option value="Joshik Sravan">Joshik Sravan</option>
+              <option value="Sadguru Sharan">Sadguru Sharan</option>
+              <option value="Keerthana">Keerthana</option>
+              <option value="Ankit">Ankit</option>
+              <option value="Gokul Vijay">Gokul Vijay</option>
+              <option value="Vinaykumar">Vinaykumar</option>
+              <option value="Aditya">Aditya</option>
+              <option value="Rahul Bumb">Rahul Bumb</option>
+              <option value="Umesh">Umesh</option>
+              <option value="Reethika">Reethika</option>
+            </select>
           </div>
         </div>
+        <button className="submit" onClick={accept}>
+          ACCEPT
+        </button>
+        <button className="decline" onClick={reject}>
+          DECLINE
+        </button>
       </div>
-      <div className="container">
-        <h3 className="questionarise">OTHER GENERAL QUESTIONS</h3>
-        <div className="questions">
-          <h3>Were you a Shaastra CA previously?</h3>
-          <div>
-            <p>{data?.getQuestionnaireByUserId.Q1 === "NO" ? "NO" : "YES"}</p>
-          </div>
-          <p></p>
-          <h3>If Your answer to the above question is yes, which year ?</h3>
-          <p>{data?.getQuestionnaireByUserId.Q1 === "NO" ? "-" : data?.getQuestionnaireByUserId.Q1}</p>
-          <h3>Which social media sites do you use? </h3>
-          <p>
-            {data?.getQuestionnaireByUserId.Q2}
-          </p>
-          <h3> Facebook Profile Link: </h3>
-          <p>
-            {data?.getQuestionnaireByUserId.Q3}
-            <link rel="stylesheet" href="styles.css"  />
-          </p>
-          <h3> Why do you wish to become a Campus Ambassador? </h3>
-          <p>
-            {data?.getQuestionnaireByUserId.Q4}
-          </p>
-          <h3>
-            Why do you think you are the right candidate for this position?
-          </h3>
-          <p>
-            {data?.getQuestionnaireByUserId.Q5}
-          </p>
-          <h3>
-            Do you have any past experience in handling Positions of
-            Responsibility?{" "}
-          </h3>
-          <p>
-            {data?.getQuestionnaireByUserId.Q6}
-          </p>
-          <label htmlFor="sfafaferfaef"></label>
-          <select onChange={async (e) => {
-            await setCoord(e.target.value)
-            console.log(coord)
-            }}>
-            <option value="Anshid">Anshid</option>
-            <option value="Samrudha Lakshmi">Samrudha Lakshmi</option>
-            <option value="Poojitha R">Poojitha R</option>
-            <option value="Sukriti">Sukriti </option>
-            <option value="Vishwa">Vishwa</option>
-            <option value="Pavan Kumar">Pavan Kumar</option>
-            <option value="Joshik Sravan">Joshik Sravan</option>
-            <option value="Sadguru Sharan">Sadguru Sharan</option>
-            <option value="Keerthana">Keerthana</option>
-            <option value="Ankit">Ankit</option>
-            <option value="Gokul Vijay">Gokul Vijay</option>
-            <option value="Vinaykumar">Vinaykumar</option>
-            <option value="Aditya">Aditya</option>
-            <option value="Rahul Bumb">Rahul Bumb</option>
-            <option value="Umesh">Umesh</option>
-            <option value="Reethika">Reethika</option>
-          </select>
-        </div>
-      </div>
-      <button className="submit" onClick={accept}>ACCEPT</button>
-      <button className="decline" onClick={reject}>DECLINE</button>
-    </div>
-  )
-  else return null
+    );
+  else return null;
 }
 
 export default Application;
--- src/MyApplication.tsx
+++ src/MyApplication.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { useContext } from "react";
 import { useHistory } from "react-router-dom";
-import "./Application.css";
+import "./MyApplication.css";
 import images1 from "./assets/demopic.jpg";
 import { useGetQuestionnaireQuery } from "./generated";
 import AuthContext from "./utils/context";
+import Header from "./Header";
 
 function MyApplication() {
   const history = useHistory();
@@ -22,105 +23,108 @@ function MyApplication() {
 
   return (
     <div className="Application">
+      <Header />
       <div>
         <h1>APPLICATION</h1>
         <div className="flexdis">
-          <div>
-            <h2>PROFILE DETAILS :</h2>
-            <h2 className="details">
-              {localStorage.getItem("name")?.toLocaleUpperCase()}
-            </h2>
-            <h2 className="details">
-              {localStorage.getItem("email")?.toLocaleUpperCase()}
-            </h2>
-          </div>
-          <div>
-            <img className="photo" src={images1} alt="" />
-          </div>
-        </div>
-        <div className="column">
-          <div>
-            <ul className="ul11">
-              <li>
-                <b>COLLEGE NAME :</b>{" "}
-                {data?.getQuestionnaire.college.toLocaleUpperCase()}
-              </li>
-              <li>
-                <b>COLLEGE ADDRESS :</b>{" "}
-                {data?.getQuestionnaire.collegeaddress.toLocaleUpperCase()}
-              </li>
-              <li>
-                <b>CITY :</b> {data?.getQuestionnaire.city.toLocaleUpperCase()}
-              </li>
-              <li>
-                <b>STATE :</b>{" "}
-                {data?.getQuestionnaire.state.toLocaleUpperCase()}
-              </li>
-              <li>
-                <b>DEGREE:</b>{" "}
-                {data?.getQuestionnaire.Degree.toLocaleUpperCase()}
-              </li>
-              <li>
-                <b>BRANCH :</b>{" "}
-                {data?.getQuestionnaire.branch.toLocaleUpperCase()}
-              </li>
-              <li>
-                <b>YEAR :</b> {data?.getQuestionnaire.year.toLocaleUpperCase()}
-              </li>
-            </ul>
-          </div>
-          <div>
-            <ul>
-              <li>
-                <b>POSTAL ADDRESS:</b>{" "}
-                {data?.getQuestionnaire.postaladdress.toLocaleUpperCase()}
-              </li>
-              <li>
-                <b>CITY :</b> {data?.getQuestionnaire.pcity.toLocaleUpperCase()}
-              </li>
-              <li>
-                <b>STATE :</b>{" "}
-                {data?.getQuestionnaire.pstate.toLocaleUpperCase()}
-              </li>
-              <li>
-                <b>PINCODE :</b>{" "}
-                {data?.getQuestionnaire.pincode.toLocaleUpperCase()}
-              </li>
-              <li>
-                <b>CONTACT NUMBER :</b>{" "}
-                {data?.getQuestionnaire.contactno.toLocaleUpperCase()}
-              </li>
-              <li>
-                <b>WHATSAPP NUMBER :</b>{" "}
-                {data?.getQuestionnaire.whatsappno.toLocaleUpperCase()}
-              </li>
-            </ul>
+          <div className="MyApplication_profile">
+            <div className="MyApplication_Pro_detail">
+              <h2>{localStorage.getItem("name")?.toLocaleUpperCase()}</h2>
+              <h3>{localStorage.getItem("email")?.toLocaleUpperCase()}</h3>
+            </div>
+            <div className="MyApplication_Address">
+              <div className="college_details">
+                <ul className="ul11">
+                  <li className="l11">
+                    <b>COLLEGE NAME :</b>
+                    {data?.getQuestionnaire.college.toLocaleUpperCase()}
+                  </li>
+                  <li className="l11">
+                    <b>COLLEGE ADDRESS :</b>
+                    {data?.getQuestionnaire.collegeaddress.toLocaleUpperCase()}
+                  </li>
+                  <li className="l11">
+                    <b>CITY :</b>{" "}
+                    {data?.getQuestionnaire.city.toLocaleUpperCase()}
+                  </li>
+                  <li className="l11">
+                    <b>STATE :</b>
+                    {data?.getQuestionnaire.state.toLocaleUpperCase()}
+                  </li>
+                  <li className="l11">
+                    <b>DEGREE:</b>
+                    {data?.getQuestionnaire.Degree.toLocaleUpperCase()}
+                  </li>
+                  <li className="l11">
+                    <b>BRANCH :</b>
+                    {data?.getQuestionnaire.branch.toLocaleUpperCase()}
+                  </li>
+                  <li className="l11">
+                    <b>YEAR :</b>
+                    {data?.getQuestionnaire.year.toLocaleUpperCase()}
+                  </li>
+                </ul>
+              </div>
+              <div className="Address_column2">
+                <ul>
+                  <li className="Address">
+                    <b>POSTAL ADDRESS:</b>
+                    {data?.getQuestionnaire.postaladdress.toLocaleUpperCase()}
+                  </li>
+                  <li>
+                    <b>CITY</b>
+                    <b>CITY :</b>{" "}
+                    {data?.getQuestionnaire.pcity.toLocaleUpperCase()}
+                  </li>
+                  <li>
+                    <b>STATE :</b>
+                    {data?.getQuestionnaire.pstate.toLocaleUpperCase()}
+                  </li>
+                  <li>
+                    <b>PINCODE :</b>
+                    {data?.getQuestionnaire.pincode.toLocaleUpperCase()}
+                  </li>
+                  <li>
+                    <b>CONTACT NUMBER :</b>
+                    {data?.getQuestionnaire.contactno.toLocaleUpperCase()}
+                  </li>
+                  <li>
+                    <b>WHATSAPP NUMBER :</b>
+                    {data?.getQuestionnaire.whatsappno.toLocaleUpperCase()}
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <h3 className="details">
+              <b>APPLICATION STATUS: </b>SELECTED
+              {localStorage.getItem("role")?.toLocaleUpperCase()}
+            </h3>
           </div>
         </div>
       </div>
-      <div className="container">
+      <div className="MyApplication_container">
         <h3 className="questionarise">OTHER GENERAL QUESTIONS</h3>
         <div className="questions">
           <h3>
-            Were you a Shaastra CA previously? If Your answer to the above
+            {" "}
+            1. Were you a Shaastra CA previously? If Your answer to the above
             question is yes, which year ?
           </h3>
           <p>{data?.getQuestionnaire.Q1}</p>
-          <h3>Which social media sites do you use? </h3>
+          <h3> 2. Which social media sites do you use? </h3>
           <p>{data?.getQuestionnaire.Q2}</p>
-          <h3> Facebook Profile Link: </h3>
-          <p>
-            {data?.getQuestionnaire.Q3}
-            <link rel="stylesheet" href="styles.css" />
-          </p>
-          <h3> Why do you wish to become a Campus Ambassador? </h3>
+          <p></p>
+          <h3> 3. Facebook Profile Link: </h3>
+          {data?.getQuestionnaire.Q3}
+          <p>{data?.getQuestionnaire.Q3}</p>
+          <h3> 4. Why do you wish to become a Campus Ambassador? </h3>
           <p>{data?.getQuestionnaire.Q4}</p>
           <h3>
-            Why do you think you are the right candidate for this position?
+            5. Why do you think you are the right candidate for this position?
           </h3>
           <p>{data?.getQuestionnaire.Q5}</p>
           <h3>
-            Do you have any past experience in handling Positions of
+            6. Do you have any past experience in handling Positions of
             Responsibility?{" "}
           </h3>
           <p>{data?.getQuestionnaire.Q6}</p>
