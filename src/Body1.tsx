e30c0c5eb69fde3965247c897af806c68c72e7aa
Prithivi-Raj <na19b052@smail.iitm.ac.in>
2021-09-23 20:29:48 +0530

    particlejs

--- src/Body1.tsx
+++ src/Body1.tsx
@@ -1,12 +1,101 @@
 import React from "react";
 import "./Body1.css";
 import { useGetQuestionnaireQuery } from "./generated";
-
+import Particles from 'react-tsparticles';
 const Body1 = () => {
   const { data } = useGetQuestionnaireQuery();
   if (data?.getQuestionnaire)
-    return (
+    return (<div>
+        <Particles
+        className='particles'
+        style={{
+           
+            position: "fixed",
+          width: "100%",
+          height: "100vh",
+          zIndex:-1,
+          top:0,
+          left:0
+        }}
+      id="tsparticles"
+      options={{ 
+        fpsLimit: 60,
+        interactivity: {
+          detectsOn: "canvas",
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 2,
+              opacity: 0.8,
+              size: 40,
+            },
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#f30a0a",
+          },
+          links: {
+            color: "#006dff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 6,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              value_area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            random: true,
+            value: 5,
+          },
+        },
+        detectRetina: true, }}
+        />  
+
+   
+      
       <div className="Bodyt">
+       
         <div className="Body_Item1t">
           <b>THANK YOU FOR APPLYING TO CAMPUS AMABASSADOR</b>
         </div>
@@ -18,9 +107,99 @@ const Body1 = () => {
           to go to CA portal
         </div>
       </div>
+      </div>
     );
 
   return (
+    <div>
+
+<Particles
+        className='particles'
+        style={{
+           
+            position: "fixed",
+          width: "100%",
+          height: "100vh",
+          zIndex:-1,
+          top:0,
+          left:0
+        }}
+      id="tsparticles"
+      options={{ 
+        fpsLimit: 60,
+        interactivity: {
+          detectsOn: "canvas",
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 2,
+              opacity: 0.8,
+              size: 40,
+            },
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#f30a0a",
+          },
+          links: {
+            color: "#006dff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 6,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              value_area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            random: true,
+            value: 5,
+          },
+        },
+        detectRetina: true, }}
+        />  
+
+   
     <div className="Bodyt">
       <div className="Body_Item1t">
         <b>THANK YOU FOR APPLYING FOR CAMPUS AMABASSADOR</b>
@@ -32,6 +211,7 @@ const Body1 = () => {
         </a>
         to fill questionaire
       </div>
+    </div>
     </div>
   );
 };
--- src/Body2.tsx
+++ src/Body2.tsx
@@ -1,8 +1,97 @@
 import React from 'react';
 import "./Body1.css"
+import Particles from 'react-tsparticles';
 
 const Selected = () => {
     return (
+        <div>
+            <Particles
+        className='particles'
+        style={{
+           
+            position: "fixed",
+          width: "100%",
+          height: "100vh",
+          zIndex:-1,
+          top:0,
+          left:0
+        }}
+      id="tsparticles"
+      options={{ 
+        fpsLimit: 60,
+        interactivity: {
+          detectsOn: "canvas",
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 2,
+              opacity: 0.8,
+              size: 40,
+            },
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#f30a0a",
+          },
+          links: {
+            color: "#006dff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 6,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              value_area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            random: true,
+            value: 5,
+          },
+        },
+        detectRetina: true, }}
+        />  
+
+       
         
         <div className="Bodyt" >
             <div className='Body_Item2t'>
@@ -12,19 +101,109 @@ const Selected = () => {
                 YOU ARE SELECTED AS A CA FOR SHAASTRA 2022. <a href="./"><span className='redt'>click here</span></a> to go to CA portal
             </div>
         </div>
+        </div>
     )
 }
 
 const Rejected = () => {
     return (
+        <div>
+
+      
         
+        <Particles
+        className='particles'
+        style={{
+           
+            position: "fixed",
+          width: "100%",
+          height: "100vh",
+          zIndex:-1,
+          top:0,
+          left:0
+        }}
+      id="tsparticles"
+      options={{ 
+        fpsLimit: 60,
+        interactivity: {
+          detectsOn: "canvas",
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 2,
+              opacity: 0.8,
+              size: 40,
+            },
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#f30a0a",
+          },
+          links: {
+            color: "#006dff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 6,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              value_area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            random: true,
+            value: 5,
+          },
+        },
+        detectRetina: true, }}
+        />  
         <div className="Bodyt" >
             <div className='Body_Item2t'>
                  <b>CONGRATULATIONS!!</b>
             </div>
             <div className='Style2t'>
-                YOU ARE SELECTED AS A CA FOR SHAASTRA 2022. <a href="./"><span className='redt'>click here</span></a> to go to CA portal
+                YOU ARE REJECTED AS A CA FOR SHAASTRA 2022. <a href="./"><span className='redt'>click here</span></a> to go to CA portal
             </div>
+        </div>
         </div>
     )
 }
