21f9817751c684cc55c0b6b13d5bc032b8f3fb32
Drishtipeshwani <archiepeshwani09@gmail.com>
2021-10-12 14:14:32 +0530

    Changed profile image

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,69 +1,74 @@
-import React from "react";
-import user from "./assets/Ranveer-Singh.jpg";
-import "./Profile.css";
-import Header from "./Header";
-import { useContext } from "react";
-import AuthContext from "./utils/context";
-import { useGetQuestionnaireQuery } from "./generated";
-import { useHistory } from "react-router-dom";
-import {Dialog, Box} from "@material-ui/core"
+import React from 'react'
+import user from './assets/profile.png'
+import './Profile.css'
+import Header from './Header'
+import { useContext } from 'react'
+import AuthContext from './utils/context'
+import { useGetQuestionnaireQuery } from './generated'
+import { useHistory } from 'react-router-dom'
+import { Dialog, Box } from '@material-ui/core'
 
 function Profile() {
-  const { role } = useContext(AuthContext);
-  const { data, error } = useGetQuestionnaireQuery();
-  const history = useHistory();
+  const { role } = useContext(AuthContext)
+  const { data, error } = useGetQuestionnaireQuery()
+  const history = useHistory()
 
   if (error) {
-    if (error.message.includes("Access denied!")) {
-        const closeHandler= () => {history.push('/login')}
-        return(
-          <Dialog onClose={closeHandler} open={true} >
-              <p>Please login to continue.</p>
-              <button onClick={closeHandler}>Close</button>
-          </Dialog>
-      );
+    if (error.message.includes('Access denied!')) {
+      const closeHandler = () => {
+        history.push('/login')
+      }
+      return (
+        <Dialog onClose={closeHandler} open={true}>
+          <p>Please login to continue.</p>
+          <button onClick={closeHandler}>Close</button>
+        </Dialog>
+      )
     }
     {
-      const closeHandler= () => {history.push('/')}
-      return(
-        <Dialog onClose={closeHandler} open={true} >
-            <p>Some error occurred.</p>
-            <button onClick={closeHandler}>Close</button>
+      const closeHandler = () => {
+        history.push('/')
+      }
+      return (
+        <Dialog onClose={closeHandler} open={true}>
+          <p>Some error occurred.</p>
+          <button onClick={closeHandler}>Close</button>
         </Dialog>
-    );}
+      )
+    }
   }
 
   return (
     <>
       <Header />
-      <div className="Profile">
-        <div className="Profile_details">
-          <div className="imgBox">
-            <img src={user} alt="" />
+      <div className='Profile'>
+        <div className='Profile_details'>
+          <div className='imgBox'>
+            <img src={user} alt='' />
           </div>
-          <div className="contentBox">
-            <h1>{localStorage.getItem("name")?.toLocaleUpperCase()}</h1>
-            <p>{localStorage.getItem("email")?.toLocaleUpperCase()}</p>
+          <div className='contentBox'>
+            <h1>{localStorage.getItem('name')?.toLocaleUpperCase()}</h1>
+            <p>{localStorage.getItem('email')?.toLocaleUpperCase()}</p>
             <p>
-              {data?.getQuestionnaire.college.toLocaleUpperCase()},{" "}
+              {data?.getQuestionnaire.college.toLocaleUpperCase()},{' '}
               {data?.getQuestionnaire.state.toLocaleUpperCase()}
             </p>
             <p>
-              {data?.getQuestionnaire.pcity.toLocaleUpperCase()},{" "}
-              {data?.getQuestionnaire.pstate.toLocaleUpperCase()} -{" "}
+              {data?.getQuestionnaire.pcity.toLocaleUpperCase()},{' '}
+              {data?.getQuestionnaire.pstate.toLocaleUpperCase()} -{' '}
               {data?.getQuestionnaire.pincode.toLocaleUpperCase()}
             </p>
           </div>
         </div>
-        <p className="Profile_status">
+        <p className='Profile_status'>
           APPLICATION STATUS : <span>{role.toLocaleUpperCase()}</span>
         </p>
-        <div className="Profile_edit">
+        <div className='Profile_edit'>
           <form></form>
         </div>
       </div>
     </>
-  );
+  )
 }
 
-export default Profile;
+export default Profile
